d3d4bdfbb8556ff4fd9214cc0d205f0e3c3005d0
Vishalmahajan1521 <139781487+Vishalmahajan1521@users.noreply.github.com>
2026-01-06 15:33:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 from app.routes import router
 
-app = FastAPI(
-    title="FastAPI MongoDB CRUD API",
-    description="CRUD operations using FastAPI and MongoDB",
-    version="1.0"
-)
+app = FastAPI()
 
 app.include_router(router)
